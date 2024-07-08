da9250307e43d8d882d2c85c9dbbeae6ba830e09
Storm21212 <122127356+Storm21212@users.noreply.github.com>
2024-07-08 11:40:22 -0400

    Delete Client Base/Client/Modules/Jesus.cpp

--- Client Base/Client/Modules/Jesus.cpp
+++
@@ -1,8 +0,0 @@
-#include "Jesus.h"
-
-void Jesus::onGmTick() {
-	LocalPlayer* Player = Minecraft::ClientInstance()->LocalPlayer();
-	if (Player != nullptr && Player->isInWater() || Player->isInLava()) {
-		Player->velocity.y = 0.25f;
-	}
-}
