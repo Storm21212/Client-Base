5e9580ec14278ac05a69a4dcdad451c81e7ae276
Storm21212 <122127356+Storm21212@users.noreply.github.com>
2024-07-08 11:39:02 -0400

    Delete Client Base/Client/Modules/AirJump.cpp

--- Client Base/Client/Modules/AirJump.cpp
+++
@@ -1,7 +0,0 @@
-#include "AirJump.h"
-
-void AirJump::onGmTick() {
-	if (Minecraft::ClientInstance()->LocalPlayer() != nullptr) {
-		Minecraft::ClientInstance()->LocalPlayer()->onGround = true;
-	}
-}
