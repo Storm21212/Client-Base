95ca80e6d19a614d5389a5caeed0ccdacc32b3c7
Storm21212 <122127356+Storm21212@users.noreply.github.com>
2024-07-08 11:41:16 -0400

    Delete Client Base/Client/Modules/Killaura.h

--- Client Base/Client/Modules/Killaura.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../../Other/Module.h"
-
-class Killaura : public Module {
-public:
-	Killaura() : Module::Module("Killaura", "Combat", "Automatically attack nearby entities") {};
-	void onGmTick();
-};
