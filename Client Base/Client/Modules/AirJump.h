6eb0fb21806493aa4a82d899727a83e0827e0a3d
Storm21212 <122127356+Storm21212@users.noreply.github.com>
2024-07-08 11:39:15 -0400

    Delete Client Base/Client/Modules/AirJump.h

--- Client Base/Client/Modules/AirJump.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../../Other/Module.h"
-
-class AirJump : public Module {
-public:
-	AirJump() : Module::Module("AirJump", "Movement", "Jump in the air") {};
-	void onGmTick();
-};
