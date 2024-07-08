385ad141e421d741706a27b46b9145983228f4b4
Storm21212 <122127356+Storm21212@users.noreply.github.com>
2024-07-08 11:39:53 -0400

    Delete Client Base/Client/Modules/Hitbox.h

--- Client Base/Client/Modules/Hitbox.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../../Other/Module.h"
-
-class Hitbox : public Module {
-public:
-	Hitbox() : Module::Module("Hitbox", "Combat", "Enlarge Enemy Hitboxes") {}
-	void onGmTick();
-	void onDisable();
-};
