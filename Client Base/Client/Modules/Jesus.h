33966b3d2bb3871e5d5cd775ea037ec52a4d1d8d
Storm21212 <122127356+Storm21212@users.noreply.github.com>
2024-07-08 11:40:30 -0400

    Delete Client Base/Client/Modules/Jesus.h

--- Client Base/Client/Modules/Jesus.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../../Other/Module.h"
-
-class Jesus : public Module {
-public:
-	Jesus() : Module::Module("Jesus", "Movement", "Automatically float above water") {};
-	void onGmTick();
-};
