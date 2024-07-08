7fe858ffdcb3e2409e52c7f1f606e174f1c6bc1d
Storm21212 <122127356+Storm21212@users.noreply.github.com>
2024-07-08 11:40:13 -0400

    Delete Client Base/Client/Modules/Instabreak.h

--- Client Base/Client/Modules/Instabreak.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../../Other/Module.h"
-
-class Instabreak : public Module {
-public:
-	Instabreak() : Module::Module("Instabreak", "World", "Instantly destroy blocks") {};
-	void onStartDestroyBlock(GameMode* GM, Vec3_i* blockPos, uint8_t blockFace);
-};
